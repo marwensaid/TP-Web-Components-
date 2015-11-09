634bc45c595177001dd5b1aabc1b4a1d3b1a4b28
marwensaid <marwensaidi1@gmail.com>
2015-11-09 13:38:23 +0100

    feat(web-comp) add view , style file and JS implementation to project legacy --MS

---
+++ js/sampleForm.js
@@ -0,0 +1,15 @@
+/**
+ * Created by marwen on 09/11/15.
+ */
+
+function soumettre() {
+    console.log('formulaire soumis');
+
+    // Soumission en Ajax, si on est là c'est que le formulaire est valide
+
+    var request = new XmlHttpRequest();
+
+    // etc.
+
+    return false;
+}
