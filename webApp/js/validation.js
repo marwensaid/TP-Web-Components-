b3a7bf81c4fe3f293d55c05dbe9d2fc416252a03
marwensaid <marwensaidi1@gmail.com>
2015-11-11 19:12:44 +0100

    feat(web-comp) add localisation and PJ & refactor package--MS

--- webApp/js/validation.js
+++ webApp/js/validation.js
@@ -1,8 +1,4 @@
-/**
- * Created by arnaud.scote on 21/03/14.
- */
-function validation()
-{
+function validation() {
 
     champNULL = "Ce champs ne peut être vide";
     input_name = window.document.getElementById("nom");
@@ -22,121 +18,96 @@ function validation()
     var pattRegexLogin = new RegExp("^[a-zA-Z0-9]{3,20}$");
     var pattRegexAdresse = new RegExp("[0-9a-zA-Z]{10,50}");
 
-    if(input_name.value != "")
-    {
-        if(!pattRegexName.test(input_name.value))
-        {
+    if (input_name.value != "") {
+        if (!pattRegexName.test(input_name.value)) {
             nom_error = window.document.getElementById("nom_error");
             retour = inputWrong(input_name, nom_error, "Ce champs doit avoir une longueur de 3 caractères minimum et 10 maximum");
         }
-        else
-        {
+        else {
             removeErrorMessage(input_name, nom_error);
         }
     }
-    else
-    {
+    else {
         retour = inputVide(input_name, nom_error);
     }
 
-    if(input_prenom.value != "")
-    {
-        if(!pattRegexPrenom.test(input_prenom.value))
-        {
+    if (input_prenom.value != "") {
+        if (!pattRegexPrenom.test(input_prenom.value)) {
             prenom_error = window.document.getElementById("prenom_error");
-            retour = inputWrong(input_prenom,prenom_error,"Ce champs doit avoir une longueur de 3 caractère minimum et 12 maximum");
+            retour = inputWrong(input_prenom, prenom_error, "Ce champs doit avoir une longueur de 3 caractère minimum et 12 maximum");
         }
-        else
-        {
-            removeErrorMessage(input_prenom,prenom_error);
+        else {
+            removeErrorMessage(input_prenom, prenom_error);
         }
     }
-    else
-    {
-        retour = inputVide(input_prenom,prenom_error);
+    else {
+        retour = inputVide(input_prenom, prenom_error);
     }
 
-   if(input_datenaiss.value != "")
-    {
-       /* if(!pattRegexPrenom.test(input_datenaiss.value))
-        {
-            datenaiss_error = window.document.getElementById("datenaiss_error");
-            retour = inputWrong(input_datenaiss,datenaiss_error,"Ce n'est pas une date valide");
-        }*/
-        removeErrorMessage(input_datenaiss,window.document.getElementById("datenaiss_error"));
+    if (input_datenaiss.value != "") {
+        /* if(!pattRegexPrenom.test(input_datenaiss.value))
+         {
+         datenaiss_error = window.document.getElementById("datenaiss_error");
+         retour = inputWrong(input_datenaiss,datenaiss_error,"Ce n'est pas une date valide");
+         }*/
+        removeErrorMessage(input_datenaiss, window.document.getElementById("datenaiss_error"));
     }
-    else
-    {
-        retour = inputVide(input_datenaiss,datenaiss_error);
+    else {
+        retour = inputVide(input_datenaiss, datenaiss_error);
     }
 
-    if(input_email.value != "")
-    {
-        if(!pattRegexEmail.test(input_email.value))
-        {
+    if (input_email.value != "") {
+        if (!pattRegexEmail.test(input_email.value)) {
             email_error = window.document.getElementById("email_error");
-            retour = inputWrong(input_email,email_error,"Adresse email non valide !");
+            retour = inputWrong(input_email, email_error, "Adresse email non valide !");
         }
-        else
-        {
-            removeErrorMessage(input_email,email_error);
+        else {
+            removeErrorMessage(input_email, email_error);
         }
     }
-    else
-    {
+    else {
         retour = inputVide(input_email, email_error);
     }
 
-    if(input_cp.value != "")
-    {
-        if(!pattRegexCp.test(input_cp.value))
-        {
+    if (input_cp.value != "") {
+        if (!pattRegexCp.test(input_cp.value)) {
             cp_error = window.document.getElementById("cp_error");
-            retour = inputWrong(input_cp,cp_error,"Code postal invalide !");
+            retour = inputWrong(input_cp, cp_error, "Code postal invalide !");
         }
-        else{
-            removeErrorMessage(input_cp,cp_error);
+        else {
+            removeErrorMessage(input_cp, cp_error);
         }
     }
-    else
-    {
+    else {
         retour = inputVide(input_cp, cp_error);
     }
 
-    if(input_adresse.value != "")
-    {
-        if(!pattRegexAdresse.test(input_adresse.value))
-        {
+    if (input_adresse.value != "") {
+        if (!pattRegexAdresse.test(input_adresse.value)) {
             adresse_error = window.document.getElementById("adresse_error");
-            retour = inputWrong(input_adresse, adresse_error,"Adresse invalide");
+            retour = inputWrong(input_adresse, adresse_error, "Adresse invalide");
         }
-        else
-        {
+        else {
             removeErrorMessage(input_adresse, adresse_error);
         }
     }
-    else
-    {
+    else {
         retour = inputVide(input_adresse, adresse_error);
     }
 
-    if(input_login.value != "")
-    {
-        if(!pattRegexLogin.test(input_login.value))
-        {
+    if (input_login.value != "") {
+        if (!pattRegexLogin.test(input_login.value)) {
             login_error = window.document.getElementById("login_error");
-            retour = inputWrong(input_login,login_error,"Ce champs doit avoir une longueur de 3 caractère minimum et 20 maximum");
+            retour = inputWrong(input_login, login_error, "Ce champs doit avoir une longueur de 3 caractère minimum et 20 maximum");
         }
     }
-    else
-    {
-        retour = inputVide(input_login,login_error);
+    else {
+        retour = inputVide(input_login, login_error);
     }
     return retour;
 }
 
-function inputWrong(input,error,message)
-{
+function inputWrong(input, error, message) {
     $(input).addClass("input-wrong");
     error.innerHTML = message;
     $(error).removeClass("hidden");
@@ -144,8 +115,7 @@ function inputWrong(input,error,message)
     return false;
 }
 
-function inputVide(input, error)
-{
+function inputVide(input, error) {
     $(input).addClass("input-wrong");
     $(error).removeClass("hidden");
     error.innerHTML = champNULL;
@@ -153,8 +123,20 @@ function inputVide(input, error)
     return false;
 }
 
-function removeErrorMessage(input, error)
-{
+function removeErrorMessage(input, error) {
     $(input).removeClass("input-wrong");
     $(error).addClass("hidden");
+}
+
+function geolocalise() {
+    console.log("geoloc");
+
+    navigator.geolocation.getCurrentPosition(displayPosition);
+}
+
+function traiteFichiers(evt) {
+    var files = event.target.files;
+
+    console.log("fichiers selectionnés : " + files.length);
+
 }
